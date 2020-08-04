6972c56bdb97f7a96e5918a3a185228773312935
afamai <alpha.mai@ryerson.ca>
2020-08-04 19:06:36 -0400

    Add randomizable polygons

--- examples/EvolvingImages/evolvingImages.js
+++ examples/EvolvingImages/evolvingImages.js
@@ -1,19 +1,51 @@
 var canvas = null;
 var context = null;
 
+class Polygon {
+    constructor(width, height) {
+        this.vertices = [];
+        this.width = width;
+        this.height = height;
+        let r = Math.round(Math.random() * 255);
+        let g = Math.round(Math.random() * 255);
+        let b = Math.round(Math.random() * 255);
+        let a = Math.random();
+        this.rgba = "rgba(" + r + ", " + g + ", " + b + ", " + a + ")";
+        this.randomize();
+    }
+
+    randomize() {
+        for(let i = 0; i < 4; i++) {
+            this.vertices.push({x: Math.random() * this.width, y: Math.random() * this.height});
+        }
+    }
+}
+
 class PolygonImage {
     constructor(width, height) {
         // create canvas
         this.canvas = new OffscreenCanvas(width, height);
         this.ctx = this.canvas.getContext("2d");
         
-        // draw a triangle
-        this.ctx.beginPath();
-        this.ctx.moveTo(50, 50);
-        this.ctx.lineTo(70, 70);
-        this.ctx.lineTo(30, 70);
-        this.ctx.lineTo(50, 50);
-        this.ctx.stroke();
+        // generate list of polygons
+        this.polygons = [];
+        let size = 100;
+        for(let i= 0; i < size; i++) {
+            this.polygons.push(new Polygon(width, height));
+        }
+
+        // draw the polygons onto the offscreen canvas
+        for(let i = 0; i < size; i++) {
+            let vertices = this.polygons[i].vertices;
+            this.ctx.fillStyle = this.polygons[i].rgba;
+            this.ctx.beginPath();
+            this.ctx.moveTo(vertices[0].x, vertices[0].y);
+            for (let j = 1; j < vertices.length; j++) {
+                this.ctx.lineTo(vertices[j].x, vertices[j].y);
+            }
+            this.ctx.lineTo(vertices[0].x, vertices[0].y);
+            this.ctx.fill();
+        }
     }
 }
 
